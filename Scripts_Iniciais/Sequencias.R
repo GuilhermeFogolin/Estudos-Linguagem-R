2c447d5b16c3ec1b6e1c6014cfefc7f4b82315f8
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-03 18:17:30 -0300

    Validando sequências com amostra aleatória

--- Scripts_Iniciais/Sequencias.R
+++ Scripts_Iniciais/Sequencias.R
@@ -11,3 +11,15 @@ rep(3,4) # Repetir o número 3 4x
 rep(1:5, 5) # Repetir sequência 5x
 rep(produtos, 3) # Repetir vetor 3x
 rep(produtos, 2, each = 3) # Repetir vetor 2x e cada elemento 3x
+
+# Com amostras aleatórias (sorteio)
+
+sample(1:30, 10) # Em um intervalo de 1:30 selecionar 10 valores
+sample(1:30, replace = F) # Sorteio sem reposição
+
+# Acessar, modificar, substituir
+
+id_produtos <- 1000:1250
+id_produtos[45]
+id_produtos[c(32, 150)]
+id_produtos[1] <- 9999
