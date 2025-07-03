c0cb0734997893243137e2466556860648c68fcb
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-03 17:52:51 -0300

    Resolvendo exercício 04 do teste 02

--- Scripts_Iniciais/Teste02.R
+++ Scripts_Iniciais/Teste02.R
@@ -8,3 +8,14 @@ prova1[prova1>7]
 prova2[prova2==7]
 prova3[c(2,4,6)]
 length(prova2[prova2!=4])
+
+# Exercício 04
+
+provaA<-c(3,6,4,2,7,8,5,6)
+provaB<-c(6,10,3,6,4,2,7,5)
+provaC<-c(10,10,5,7,4,6,2,7)
+
+sum(provaA, provaB, provaC)
+100 * ((provaB - provaA) / provaA)
+mean(provaA)
+provaA < provaB
