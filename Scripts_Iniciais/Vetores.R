de3d93a096e503e0426cef7f56c8b58e0ccab7a9
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-03 14:16:44 -0300

    Modificando valores em um vetor

--- Scripts_Iniciais/Vetores.R
+++ Scripts_Iniciais/Vetores.R
@@ -17,3 +17,23 @@ vendasTotais
 vendas[1] # Por índice
 vendas[c(1,3)] # Mais de uma posição
 vendas[1:3] # Por intervalo
+
+# Modificação
+
+# Substituição
+
+vendas[2] <- 13
+
+vendas[c(1,4)] <- c(23, 34) # Mais de uma posição
+
+vendas[2:4] <- c(23, 45, 77) # Por intervalo
+
+# Remoção
+
+vendas <- vendas[-4] # Removendo o último valor
+
+# Inclusão
+
+vendas[4] <- 70 # Incluindo um valor
+
+vendas[5:7] <- c(30, 06, 25) # Incluindo valores em intervalo
