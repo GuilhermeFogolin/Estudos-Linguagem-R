cc39a360251336920bb9484216c4b8c7665499ba
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-03 18:01:08 -0300

    Resolvendo aplicação prática 2

---
+++ Scripts_Iniciais/AplicacaoPratica02.R
@@ -0,0 +1,17 @@
+# Aplicação prática 02
+
+# Questão 01
+
+nomes <- c("Marcos de Aguiar", "Paula Miranda", "Emilio Santos", "Priscila de Albuquerque")
+
+# Questão 02
+
+nomes[4] <- "Pryscilla de Albuquerque"
+
+# Questão 03
+
+salarios <- c(4000, 2500, 1200, 3400)
+
+# Questão 04
+
+salarios <- salarios - (salarios * 0.3)
