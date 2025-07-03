f95202c8b2fc34c41e6e70096261fccd0f9ea634
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-03 09:36:34 -0300

    Implementando R como calculadora

---
+++ Scripts_Iniciais/Calculadora.R
@@ -0,0 +1,38 @@
+# R como calculadora
+
+# Soma
+
+4 + 6
+
+# Subtração
+
+12 - 5
+
+# Divisão
+
+15 / 7
+
+# Multiplicação
+
+4 * 6
+
+# Resto da divisão
+
+5 %% 4
+
+# Parte inteira da divisão
+
+7 %/% 3
+
+# Potência
+
+4 ^ 2
+4 ** 2
+
+# Funções prontas
+
+log(2)
+factorial(5)
+abs(-13)
+sin(90)
+cos(180)
