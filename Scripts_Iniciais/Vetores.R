b51028641a866fd90d4d6327d2035f3867f09d52
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-03 10:24:55 -0300

    Acessando vetores

--- Scripts_Iniciais/Vetores.R
+++ Scripts_Iniciais/Vetores.R
@@ -7,3 +7,13 @@ vendas <-c(12, 15, 9, 7)
 meses <-c("JAN", "FEV", "MAR", "ABR")
 
 vendasTotais <- scan()
+
+# Acesso
+
+vendas
+meses
+vendasTotais
+
+vendas[1] # Por índice
+vendas[c(1,3)] # Mais de uma posição
+vendas[1:3] # Por intervalo
