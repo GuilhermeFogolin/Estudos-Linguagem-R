5a9de9a12dffec07281a1c82920c50ab837f5db1
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-03 17:40:05 -0300

    Criando operações com vetores booleanos

---
+++ Scripts_Iniciais/VetorBooleano.R
@@ -0,0 +1,13 @@
+# Vetor booleano
+
+produtos <- c("TV","Mouse","Notebook","Teclado")
+qtde <- c(15,32,12,21)
+
+qtde > 10 # Maior
+qtde < 21 # Menor
+qtde >= 21 # Maior igual
+qtde == 23 # Igual
+qtde != 25 # Diferente
+qtde[3] == 10 # Na posição 3 há 10 unidades?
+produtos == "Teclado" # Nos produtos existe teclado?
+sum(produtos == "TV") # Quantas vezes TV aparece?
