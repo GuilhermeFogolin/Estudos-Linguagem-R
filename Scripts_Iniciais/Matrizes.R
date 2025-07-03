c59d292945aa160f3391050afb4dda89f13d9fee
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-03 18:43:54 -0300

    Acessando os elementos da matriz

--- Scripts_Iniciais/Matrizes.R
+++ Scripts_Iniciais/Matrizes.R
@@ -11,3 +11,11 @@ vendas <- rbind(produtos, qtde) # Em linhas
 matrix(1:20, nrow = 4, ncol = 5) # Matriz com 4 linhas, 5 colunas
 matrix("Oi", nrow = 3, ncol = 3) # Matriz 3x3 somente com "Oi"
 matrix(1:10, nrow = 5, ncol = 2, byrow = T) # Ajusta contagem por linhas
+
+# Acessando
+
+vendas[2,2] # Linha | Coluna
+vendas[1,] # Toda a linha 1
+vendas[,2] # Toda a coluna 2
+vendas[2:4,] # Busca na linha 2 até 4 (com todas as colunas)
+vendas[2:4, 1] # Busca na linha 2 até 4 (somente na coluna 1)
