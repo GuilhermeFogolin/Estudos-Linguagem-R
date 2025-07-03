cb48b272d7f5e2feefbe7d7c4ff06798f48e83d4
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-03 18:09:57 -0300

    Criando sequências com elementos e repetição

---
+++ Scripts_Iniciais/Sequencias.R
@@ -0,0 +1,13 @@
+# Sequências
+
+# Criação com elementos
+
+1:20 # Forma mais simples de 1 em 1
+seq(1, 25, 2) # Começa em 1 e vai até 25 em passos de 2
+
+# Criação com repetição
+
+rep(3,4) # Repetir o número 3 4x
+rep(1:5, 5) # Repetir sequência 5x
+rep(produtos, 3) # Repetir vetor 3x
+rep(produtos, 2, each = 3) # Repetir vetor 2x e cada elemento 3x
