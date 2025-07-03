4bfa9926bf39153803bb3c7dd02246b8eefc7494
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-03 17:46:25 -0300

    Resolvendo o teste 02

---
+++ Scripts_Iniciais/Teste02.R
@@ -0,0 +1,10 @@
+# Teste 02: Exercícios com vetores
+
+prova1<-c(8,9,5,6,7,8,8,8,5)
+prova2<-c(2,3,4,3,6,4,2,9,5)
+prova3<-c(1,6,2,9,1,10,2,9,2)
+
+prova1[prova1>7]
+prova2[prova2==7]
+prova3[c(2,4,6)]
+length(prova2[prova2!=4])
