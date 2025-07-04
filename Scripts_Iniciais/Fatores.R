d73d0d150da539bd91c1fe4284ca19574a3e7c55
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-04 15:48:24 -0300

    Acessando e modificando fatores

--- Scripts_Iniciais/Fatores.R
+++ Scripts_Iniciais/Fatores.R
@@ -12,3 +12,15 @@ str(generoFator)
 
 a <- factor(c("2", "1", "2", "2", "1", "3")) # Criando diretamente
 str(a)
+
+# Acesso
+
+generoFator[2] # Segunda posição
+
+generoFator[1:3] # Intervalo
+
+generoFator[length(generoFator)] # Última posição
+
+# Modificação
+
+generoFator[1] <- 3 # Alterando na posição 1
