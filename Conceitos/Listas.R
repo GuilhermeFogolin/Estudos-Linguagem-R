23afda7be969c0f3aee09fdcadbebc33578f0b5d
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-04 19:24:20 -0300

    Nomeando elementos da lista

--- Conceitos/Listas.R
+++ Conceitos/Listas.R
@@ -14,3 +14,13 @@ balanço <- list(idCliente, clientes, qtde, novoCliente)
 # Com o comando list
 
 list(id = seq(101, 104), nomes = c("Fogolin", "Carlos", "José", "Amaro"), qtde = c(12, 2, 5, 8))
+
+# Nomeando elementos
+
+# Criando lista já com nomes
+
+balanço2 <- list(ID = idCliente, Nomes = clientes, Quantidades = qtde, Cliente_Novo = novoCliente)
+
+# Nomeando depois de criado
+
+names(balanço) <- c("ID", "Nomes", "Quantidades", "Cliente Novo?")
