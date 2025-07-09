12c05a5c11fe9542e5d96b3b4d12c1d3dd981176
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-07-09 10:53:03 -0300

    Modificando elementos em dataframe

--- Conceitos/DataFrame.R
+++ Conceitos/DataFrame.R
@@ -36,3 +36,9 @@ resumoFinal <- data.frame(fornecedores, qtde_Jan, qtde_Fev, qtde_Mar)
 str(resumoFinal)
 
 resumoFinal$qtde_Jan <- as.character(resumoFinal$qtde_Jan) # Transformando em caractere
+
+resumoFinal$qtde_Jan [5] <- "100" # Mudando um valor
+
+resumoFinal$qtde_Mar[1:3] <- c(1, 2, 3) # Sequência de valores
+
+resumoFinal$qtde_Fev[c(1, 3)] <- c(27, 28) # Valor aleatório
